1f92e43d65ec59c3e52814fa5b60c8e62a39f1fc
melihyaman <83470990+melihyaman@users.noreply.github.com>
2024-12-15 22:01:39 +0300

    1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebProgramlama.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
